5eaecd334d213a6adf4b771a1a8e7994377b99de
waseemdemp-cloud <waseem.demp@gmail.com>
2025-11-10 12:46:16 +0500

    jenkins file modified

---
+++ script.groovy
@@ -0,0 +1,15 @@
+def buildApp () {
+    echo "building the application..."
+}
+
+def testApp () {
+    echo "testing the application..."
+}
+
+def deplyApp () {
+    
+    echo "Deploying the application..."
+    echo "deploying version ${params.VERSION} to selected environment"
+}
+
+retunr this
